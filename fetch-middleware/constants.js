f3b9150cb09b1767f1ecf4b7d2814ce88e2ab396
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-04-02 01:25:53 -0700

    Basic handling of env vars close #4

---
+++ fetch-middleware/constants.js
@@ -0,0 +1,4 @@
+const EVENTBRITE_BASE_URL = 'https://www.eventbriteapi.com/v3/events/search/';
+const EVENTBRITE_QUERY_PARAMS = '?sort_by=date&location.address=1451+7th+St%2C+Oakland%2C+CA+94607&location.within=50mi&venue.region=CA&categories=103&start_date.range_start=';
+// const EVENTBRITE_KEY = process.env.EVENTBRITE_KEY;
+// https://www.eventbriteapi.com/v3/events/search/?sort_by=date&location.address=El+Cerrito+CA+94530&location.within=50mi&categories=103&token=MD33DX7LJOIGAGCBYRF7
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.NODE_ENV || 5003;
+const EVENTBRITE_KEY = process.env.EVENTBRITE_KEY;
+
+
 
 app.use('/', (req, res) => {
   res.send('working');
@@ -8,4 +11,5 @@ app.use('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
+  console.log(EVENTBRITE_KEY);
 });
